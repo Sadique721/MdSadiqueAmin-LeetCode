ba9f5e9f558ae751a1ee29d82fd6378cec857aaa
Md Sadique Amin <162752803+Sadique721@users.noreply.github.com>
2026-06-24 13:03:24 +0530

    Time: 16 ms (42.04%), Space: 93.8 MB (26.79%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            // If the set already contains the number, we found a duplicate
+            if (seen.contains(num)) {
+                return true; 
+            }
+            // Otherwise, add the number to the set
+            seen.add(num);
+        }
+        // If we finish the loop, no duplicates were found
+        return false;
+    }
+}
